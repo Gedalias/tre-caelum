209f423ebcb529e3e8268df34b1839b4e10586b5
gedas <gedaliasfreire@gmail.com>
2013-11-11 14:50:11 -0300

    teste poesia

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ class Hello{
     public static void main(String args[]) {
         System.out.println("Hello!");
 	System.out.println("Novo Olá");
+	System.out.println("Batatinha quando nasce...");
 	}
 }
